3d1aa99d87a4dff0cc396ef7f95cb996db64fe94
gurdian0614 <ivan.gurdian@outlook.com>
2025-11-19 18:10:28 -0600

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductosAppQ42025;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductosAppQ42025;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
